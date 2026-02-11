ba34837c9744d7af735f1714160fa09443b45dad
Calin Ilie <calin@MacBook-Pro-Calin.local>
2026-02-11 19:44:54 +0200

    Correction-v3

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -17,7 +17,7 @@ variable "my_ip_cidr" {
 variable "public_key_path" {
   type        = string
   description = "Path to public key for SSH connection"
-  default     = "Terraform/keys/proj-key.pub"
+  default     = "keys/proj-key.pub"
 }
 
 
