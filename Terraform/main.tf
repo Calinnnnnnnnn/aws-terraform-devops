91bd74fe349d42b89ee7abf126ebc4178f03a3a4
Calin Ilie <calin@192.168.1.6>
2026-02-15 18:45:03 +0200

    Open Grafana and Prometheus ports

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -100,6 +100,22 @@ resource "aws_security_group" "web_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "All outbound"
     from_port   = 0
